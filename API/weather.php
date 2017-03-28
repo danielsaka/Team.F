5ed4535633c55a3346a6b543074e7e54692daac2
Timur Misharin <timisharin@gmail.com>
2017-03-28 21:10:55 +0300

    API added info

---
+++ API/weather.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Weather API
+ * @see https://www.apixu.com/my/
+ * @author Timur Misharin
+ * version: 1.0
+ * @return weather dict
+ */
+
+function weather_api()
+{
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, 'http://api.apixu.com/v1/current.json?key=3532b41d2b5e44e698b173813172803&q=Ashqelon');
+    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+    $query = curl_exec($curl_handle);
+    $weather = json_decode($query);
+    print_r($weather);
+    //$string = json_decode($query);
+    //print_r($string);
+    curl_close($curl_handle);
+    return $weather;
+}
