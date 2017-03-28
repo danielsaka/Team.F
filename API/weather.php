551fbc90fc50e75b7a8ee19501641f2662ec990c
Timur Misharin <timisharin@gmail.com>
2017-03-28 21:24:24 +0300

    Wiki API added

--- API/weather.php
+++ API/weather.php
@@ -17,8 +17,23 @@ function weather_api()
     $query = curl_exec($curl_handle);
     $weather = json_decode($query);
     print_r($weather);
-    //$string = json_decode($query);
-    //print_r($string);
+    $string = json_decode($query);
+    print_r($string);
     curl_close($curl_handle);
-    return $weather;
+    return $string;
+}
+
+function wiki_api(){
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, 'https://en.wikipedia.org/w/api.php');
+    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+    $query = curl_exec($curl_handle);
+    $wiki = json_decode($query);
+    print_r($wiki);
+    $string = json_decode($query);
+    print_r($string);
+    curl_close($curl_handle);
+    return $string;
 }
